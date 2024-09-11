63cbbcb83b971e197212bfe842f5ecf06de39fd6
Abhishek Singh <130927579+abhisheksingh5680@users.noreply.github.com>
2024-09-11 13:18:49 +0530

    Update result.jsx

--- src/Components/result.jsx
+++ src/Components/result.jsx
@@ -178,9 +178,9 @@ const Result = () => {
                             <h3>Name</h3>
 
                             <div className="ans">
-{/*                                 <input type="text" placeholder='Your answer' pattern="[A-Za-z]*" oninvalid="alert('Please enter only alphabets.');" name="customer" value={inputs.customer || ""}
-                                    onChange={handleChange} required /> */}
-                               <input type="text" pattern="[a-zA-Z]+"  placeholder="Enter only alphabets" oninvalid="alert('Please enter only alphabets.'); onChange={handleChange} ">
+                                <input type="text" placeholder='Your answer' pattern="[A-Za-z]*" oninvalid="alert('Please enter only alphabets.');" name="customer" value={inputs.customer || ""}
+                                    onChange={handleChange} required />
+{/*                                <input type="text" pattern="[a-zA-Z]+"  placeholder="Enter only alphabets" oninvalid="alert('Please enter only alphabets.'); onChange={handleChange} "> */}
 
                             </div>
                         </div>
