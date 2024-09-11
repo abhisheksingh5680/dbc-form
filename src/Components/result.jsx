ba660336718f090b858a5d15f7a89622f03c848a
Abhishek Singh <130927579+abhisheksingh5680@users.noreply.github.com>
2024-09-11 13:00:22 +0530

    Update result.jsx

--- src/Components/result.jsx
+++ src/Components/result.jsx
@@ -180,6 +180,7 @@ const Result = () => {
                             <div className="ans">
                                 <input type="text" placeholder='Your answer' pattern="[A-Za-z]*" name="customer" value={inputs.customer || ""}
                                     onChange={handleChange} required />
+                                <span id="errorAlert" style="color: red;"></span> 
                             </div>
                         </div>
                     </div>
